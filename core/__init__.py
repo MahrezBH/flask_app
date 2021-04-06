aacfcf9241f8c5e0803a5153762d630158badaaa
mahrezbh <mahrez.benhamad@gmail.com>
2021-04-06 21:57:04 +0100

    just uncomment the configs

--- core/__init__.py
+++ core/__init__.py
@@ -5,10 +5,10 @@ import os
 app = Flask(__name__)
 
 # set the configs
-# if os.environ.get("SETTING") == "PROD":
-app.config.from_object('config.ProductionConfig')
-# else:
-    # app.config.from_object('config.LocalConfig')
+if os.environ.get("SETTING") == "PROD":
+    app.config.from_object('config.ProductionConfig')
+else:
+    app.config.from_object('config.LocalConfig')
 
 # import core views
 from core import views
